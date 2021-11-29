b823fdba47dbe58446540c223e01d84a85f905fe
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-11-29 02:41:23 -0600

    Redirecicon a la ruta cliente

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,8 +26,8 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
-       // protected $redirectTo = '/cliente';
+    // protected $redirectTo = RouteServiceProvider::HOME;
+       protected $redirectTo = '/cliente';
 
     /**
      * Create a new controller instance.
